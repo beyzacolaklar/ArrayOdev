6cb3f11434f9bbcda389d9faf9eeb4c46ad86802
beyzacolaklar <beyzacola@gmail.com>
2025-03-22 15:13:57 +0300

    Proje dosyası ekle.

---
+++ Array/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] sayilar = new int[5];
+        int toplam = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Lütfen {i + 1}. sayıyı girin: ");
+            sayilar[i] = Convert.ToInt32(Console.ReadLine());  
+            toplam += sayilar[i];  
+        }
+
+        
+        double ortalama = toplam / 5.0;
+
+      
+        Console.WriteLine($"Girdiğiniz sayıların ortalaması: {ortalama}");
+    }
+}
+
